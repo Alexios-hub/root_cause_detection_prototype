343beef951b4b898701628336609607e8b48f8dd
tangt813201 <tang813201@163.com>
2022-05-31 15:57:54 +0800

    更新要求again

--- src/views/RootType.vue
+++ src/views/RootType.vue
@@ -11,16 +11,66 @@
       </el-dialog>
 
     </el-row>
-    <div id="myChart1" style="width: auto;  height: 800px;"></div>
-    <div id="myChart2" style="width: auto;  height: 800px;"></div>
-    <div id="myChart3" style="width: auto;  height: 800px;"></div>
-    <div id="myChart4" style="width: auto;  height: 800px;"></div>
-    <div id="myChart5" style="width: auto;  height: 800px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[0]" @change="onRadioChange(1)" v-for="(item) in kpi1" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart1" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[1]" @change="onRadioChange(2)" v-for="(item) in kpi2" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart2" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[2]" @change="onRadioChange(3)" v-for="(item) in kpi3" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart3" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[3]" @change="onRadioChange(4)" v-for="(item) in kpi4" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart4" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[4]" @change="onRadioChange(5)" v-for="(item) in kpi5" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart5" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[5]" @change="onRadioChange(6)" v-for="(item) in kpi6" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
     <div id="myChart6" style="width: auto;  height: 800px;"></div>
-    <div id="myChart7" style="width: auto;  height: 800px;"></div>
-    <div id="myChart8" style="width: auto;  height: 800px;"></div>
-    <div id="myChart9" style="width: auto;  height: 800px;"></div>
-    <div id="myChart10" style="width: auto;  height: 800px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[6]" @change="onRadioChange(7)" v-for="(item) in kpi7" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart7" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[7]" @change="onRadioChange(8)" v-for="(item) in kpi8" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart8" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[8]" @change="onRadioChange(9)" v-for="(item) in kpi9" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart9" style="width: auto;  height: 600px;"></div>
+    <div style="margin-top:20px;">
+      <el-radio-group v-model="select[9]" @change="onRadioChange(10)" v-for="(item) in kpi10" :key="item">
+        <el-radio :label="item"  style="margin:10px">{{item}}</el-radio>
+      </el-radio-group>
+    </div>
+    <div id="myChart10" style="width: auto;  height: 600px;"></div>
   </div>
     <!-- </div>
 </div> -->
@@ -28,13 +78,70 @@
 
 <script>
 import * as echarts from 'echarts'
-import cmdbList from '../data/totalCmdbId.json'
 import seriesList from '../data/totalSeriesData.json'
 
 export default {
   name: "RootType",
   data() {
     return {
+      select:[
+        "istio_request_bytes.grpc.200.0.0",
+        "istio_request_duration_milliseconds.grpc.200.0.0",
+        "istio_request_messages",
+        "istio_requests.grpc.200.0.0",
+        "istio_response_bytes.grpc.200.0.0",
+        "istio_response_messages",
+        "istio_tcp_connections_closed.-",
+        "istio_tcp_connections_opened.-",
+        "istio_tcp_received_bytes.-",
+        "istio_tcp_sent_bytes.-",
+      ],
+      kpi1:[
+        "istio_request_bytes.grpc.200.0.0",
+        "istio_request_bytes.grpc.200.9.0",
+        "istio_request_bytes.grpc.200.14.0",
+        "istio_request_bytes.grpc.0.2.0",
+        "istio_response_messages",
+      ],
+      kpi2: [
+        "istio_request_duration_milliseconds.grpc.200.0.0",
+        "istio_request_duration_milliseconds.grpc.200.9.0",
+        "istio_request_duration_milliseconds.grpc.200.14.0",
+        "istio_request_duration_milliseconds.grpc.0.2.0"
+      ],
+      kpi3: [
+        "istio_request_messages"
+      ],
+      kpi4:[
+        "istio_requests.grpc.200.0.0",
+        "istio_requests.grpc.200.9.0",
+        "istio_requests.grpc.200.14.0",
+        "istio_requests.grpc.0.2.0"
+      ],
+      kpi5:[
+        "istio_response_bytes.grpc.200.0.0",
+        "istio_response_bytes.grpc.200.9.0",
+        "istio_response_bytes.grpc.200.14.0",
+        "istio_response_bytes.grpc.0.2.0"
+      ],
+      kpi6:[
+        "istio_response_messages"
+      ],
+      kpi7: [
+        "istio_tcp_connections_closed.-"
+      ],
+      kpi8:[
+        "istio_tcp_connections_opened.-"
+      ],
+
+      kpi9: [
+        "istio_tcp_received_bytes.-"
+      ],
+      kpi10: [
+        "istio_tcp_sent_bytes.-"
+      ],
+
+
       dialogVisible:false,
       fileName: [
         "istio_request_bytes",
@@ -69,16 +176,53 @@ export default {
   //数据自动刷新，必然需要一个监听机制告诉Echarts重新设置数据
 
   methods: {
+    onRadioChange(temp)
+    {
+      this.changeEcharts(temp);
+    },
+    changeEcharts(temp)
+    {
+      let name="myChart"+temp;
+      let myChart = echarts.init(document.getElementById(name));
+
+      var option
+      option = {
+        title: {
+          text: this.fileName[temp-1]
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        grid: {
+          top: '6%',
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: seriesList[this.fileName[temp-1]][this.select[temp-1]]
+      }
+      myChart.setOption(option)
+
+    },
     init() {
       // eslint-disable-next-line no-unused-vars
       var option
       var name
-      var patternName
       for (let i = 0; i < this.fileName.length; i++) {
-        patternName=this.fileName[i]
         name='myChart'+(i+1)
-        console.log(seriesList[patternName])
-        console.log(cmdbList[patternName])
         var myChart = echarts.init(document.getElementById(name));
         option = {
           title: {
@@ -87,12 +231,8 @@ export default {
           tooltip: {
             trigger: 'axis'
           },
-          legend: {
-            data:cmdbList[patternName],
-            top:"6%"
-          },
           grid: {
-            top: '30%',
+            top: '6%',
             left: '3%',
             right: '4%',
             bottom: '3%',
@@ -110,16 +250,12 @@ export default {
           yAxis: {
             type: 'value'
           },
-          series: seriesList[patternName]
+          series: seriesList[this.fileName[i]][this.select[i]]
         }
-        console.log(option)
+
         myChart.setOption(option)
-        // window.onresize = () => {
-        //   return (() => {
-        //     myChart.resize()
-        //   })()
-        // }
       }
+
 
       },
     goToShowTrace() {
